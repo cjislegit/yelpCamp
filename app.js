c11d9c48574c6b63696e8f48cab08e1deedbc2d0
cjislegit <carlosjramirez11@gmail.com>
2019-01-18 16:53:59 -0800

    Added two views and created routs in app.js

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('This is home');
+  res.render('landing');
+});
+
+app.get('/campgrounds', (req, res) => {
+  const campgrounds = [
+    {
+      name: 'Salmon Creek',
+      image:
+        'https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+    },
+    {
+      name: 'Granite Hill',
+      image:
+        'https://images.unsplash.com/photo-1534187886935-1e1236e856c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=670&q=80'
+    },
+    {
+      name: "Montain Goat's Rest",
+      image:
+        'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
+    }
+  ];
+  res.render('campgrounds', { campgrounds: campgrounds });
 });
 
 app.listen(3000, () => {

3b04848b0342d6b89da6095c7a4d64b6626735ab
cjislegit <carlosjramirez11@gmail.com>
2019-01-18 21:58:00 -0800

    Added form to add more campsites

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+const campgrounds = [
+  {
+    name: 'Salmon Creek',
+    image:
+      'https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+  },
+  {
+    name: 'Granite Hill',
+    image:
+      'https://images.unsplash.com/photo-1534187886935-1e1236e856c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=670&q=80'
+  },
+  {
+    name: "Montain Goat's Rest",
+    image:
+      'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
+  }
+];
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    {
-      name: 'Salmon Creek',
-      image:
-        'https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
-    },
-    {
-      name: 'Granite Hill',
-      image:
-        'https://images.unsplash.com/photo-1534187886935-1e1236e856c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=670&q=80'
-    },
-    {
-      name: "Montain Goat's Rest",
-      image:
-        'https://images.unsplash.com/photo-1526491109672-74740652b963?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
-    }
-  ];
   res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+app.post('/campgrounds', (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
 });
 
 app.listen(3000, () => {
